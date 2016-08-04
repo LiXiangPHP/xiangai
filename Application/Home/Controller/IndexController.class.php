c102617614b728c1c05d1595c2d5c94dea5f926c
zjh <923156237@qq.com>
2016-08-04 15:15:42 +0800

    增加自动登录功能

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,8 +6,8 @@ class IndexController extends Controller {
         $this->display();
     }
     function login(){
-    	$_COOKIE['user']=NULL;
-    	$_SESSION['user']=null;
+    	$_COOKIE['user']=NULL;	
+        
     	$password=empty($_POST['password'])?'':trim($_POST['password']);
     	$username=empty($_POST['username'])?'':trim($_POST['username']);
     	$password=md5($password);
@@ -15,48 +15,18 @@ class IndexController extends Controller {
     	$where=array('username'=>$username,'password'=>$password);
     	$res=$model->where($where)->find();
     	if (!empty($res)) {
-    		$_SESSION['user']=array('username'=>$username);   	
-            // if($_POST['autoLoSgin']){
-            //     $_COOKIE['user']=array('username'=>$username); 
-            // }	
-    	}
+    		$_SESSION['user']=array('username'=>$username);  
+            if (isset($_POST['autoLogin'])) {
+                setcookie('user', $username, time()+360000000,'/');  
+             }      	
+    	}  	
 		header("location:".U('index'));
 
     }
     function logout(){
-    	$_SESSION['user']=null;
+    	$_SESSION['user']=null;  
+        setcookie('user','',time()-10,'/');  
     	header("location:".U('index'));
-    }
-	 public function registerDoit() {
-    	$loginfo=I('post.loginInfo', '', 'trim');
-    	$password=I('post.password', '', 'trim');
-		if(empty($loginfo)){
-			$this->error('帐号不能为空！');
-		}
-		if(empty($password)){
-			$this->error('密码不能为空！');
-		}
-    	$model=M('users');
-		$user=$model->where('username='.$loginfo)->fetchsql(false)->select();
-		$loginfolen=strlen($loginfo);
-		$passwordlen=strlen($password);
-		if($loginfolen!=11){
-			$this->error('手机号输入有误，须11位');
-		}
-		if($passwordlen<6){
-			$this->error('密码不能少于6位');
-		}
-		if(!empty($user)){
-			$this->error('帐号已存在，请重新输入');
-		}
-		$_POST['regtime']=date('Y-m-d H:i:s');
-		$_POST['username']=$loginfo;
-    	$_POST['password']=md5($_POST['password']);
-    	if($model->add($_POST)) {
-    		$this->success('注册成功');
-    	} else {
-    		$this->error('注册失败');
-    	}
     }
 
     
