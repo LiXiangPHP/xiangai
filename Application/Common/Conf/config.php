d8e446b8ca8f019e2b1e7329200e7cec8013710a
BLN <praise777@163.com>
2016-08-04 08:55:08 +0800

    修改配置

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -54,12 +54,12 @@ return  array(
 
     /* 数据库设置 */
     'DB_TYPE'               =>  'MySQL',     // 数据库类型
-    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
-    'DB_NAME'               =>  'shop',          // 数据库名
+    'DB_HOST'               =>  '121.40.30.118', // 服务器地址
+    'DB_NAME'               =>  'xaw',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '111',          // 密码
+    'DB_PWD'                =>  'xaw~!@',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
-    'DB_PREFIX'             =>  'shop_',    // 数据库表前缀
+    'DB_PREFIX'             =>  '',    // 数据库表前缀
     'DB_PARAMS'          	=>  array(), // 数据库连接参数    
     'DB_DEBUG'  			=>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
     'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
